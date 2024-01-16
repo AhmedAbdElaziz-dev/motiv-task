f06e7d6ceec639e712b30d5c250664aabcfdd007
ahmed.abedaziz <a.abdelaziz.hamed@gmail.com>
2024-01-17 01:00:59 +0200

    Enable routing using react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Layout from "./components/Layout";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 
 function App() {
-  return <div>Hello world</div>;
+  const routeDefinitions = createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Layout />}></Route>
+    </Route>
+  );
+  const router = createBrowserRouter(routeDefinitions);
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
 }
 
 export default App;
