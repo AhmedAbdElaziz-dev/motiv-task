d51542fd9b918447bdc5a3c7c7c42ab277f0cd9c
ahmed.abedaziz <a.abdelaziz.hamed@gmail.com>
2024-01-19 20:48:17 +0200

    Add: items to sidebar

--- src/components/SideBar/constants.js
+++ src/components/SideBar/constants.js
@@ -1,15 +1,30 @@
-import CarIcon from "../../assets/Vector.png";
-import DashboardIcon from "../../assets/dashboard.png";
+import CarIcon from '../../assets/Vector.png';
+import DashboardIcon from '../../assets/dashboard.png';
+import LogoutIcon from '../../assets/Line.png';
+import SettingsIcon from '../../assets/dashboard.png';
 
-export const sidebarItems = [
+export const sidebarTopItems = [
   {
     icon: DashboardIcon,
-    label: "Dashboard",
-    to: "/",
+    label: 'Dashboard',
+    to: '/',
   },
   {
     icon: CarIcon,
-    label: "Cars",
-    to: "/search",
+    label: 'Cars',
+    to: '/search',
+  },
+];
+
+export const sidebarBottomItems = [
+  {
+    icon: SettingsIcon,
+    label: 'Settings',
+    to: '/Settings',
+  },
+  {
+    icon: LogoutIcon,
+    label: 'Logout',
+    to: '/logout',
   },
 ];
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { sidebarItems } from './constants.js';
+import { sidebarBottomItems, sidebarTopItems } from './constants.js';
 import primaryLogoImg from '../../assets/logo.png';
 import secondaryLogoImg from '../../assets/logo1.png';
 
@@ -19,17 +19,19 @@ const Sidebar: React.FC<ISidebarProps> = ({ isCollapse, setIsCollapse }) => {
   return (
     <div
       className={`z-10 bg-white sidebar-transition fixed flex h-full flex-col justify-between border-r-2 border-slate-100 px-4 ${
-        isCollapse ? 'w-[100px]' : 'w-fit'
+        isCollapse ? 'w-[80x]' : 'w-fit'
       }`}
     >
-      <ul className="flex flex-col items-center">
-        <li className="flex justify-center mt-8 mb-16">
+      <ul className="flex flex-col items-start">
+        <li
+          className={`flex justify-center mt-8 mb-16 ${isCollapse ? 'ml-2' : ''}`}
+        >
           <img src={isCollapse ? secondaryLogoImg : primaryLogoImg} alt="" />
         </li>
-        {sidebarItems.map((item, index) => (
+        {sidebarTopItems.map((item, index) => (
           <li
             key={index}
-            className={'flex justify-start p-2 mb-5 max-md:text-sm'}
+            className={'flex  p-2 mb-5 max-md:text-sm'}
             onClick={() => navigate(item.to)}
           >
             <img src={item.icon} className="h-6 w-6 max-md:w-4 mr-2" alt="" />
@@ -40,12 +42,22 @@ const Sidebar: React.FC<ISidebarProps> = ({ isCollapse, setIsCollapse }) => {
         ))}
       </ul>
 
-      <div className="w-20 mt-5 p-2">
-        <img src="./src/assets/profile.jpg" className="rounded-full" alt="" />
-      </div>
-
+      <ul className="flex flex-col items-start">
+        {sidebarBottomItems.map((item, index) => (
+          <li
+            key={index}
+            className={'flex  p-2 mb-5 max-md:text-sm'}
+            onClick={() => navigate(item.to)}
+          >
+            <img src={item.icon} className="h-6 w-6 max-md:w-4 mr-2" alt="" />
+            <label className={isCollapse ? 'hidden' : 'text-animate'}>
+              {item.label}
+            </label>
+          </li>
+        ))}
+      </ul>
       <div
-        className={` arrow-animate absolute  ${isCollapse ? 'left-20' : 'left-36'}  top-10 z-10 flex h-[30px] w-[30px] items-center justify-center rounded-full bg-gray-100 hover:cursor-pointer 
+        className={` arrow-animate absolute  ${isCollapse ? 'left-16' : 'left-36'}  top-10 z-10 flex h-[30px] w-[30px] items-center justify-center rounded-full bg-gray-100 hover:cursor-pointer 
           ${isCollapse ? '' : 'rotate-arrow'}`}
         onClick={handleSidebarCollapse}
       >
